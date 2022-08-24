c5642381aa5d1028c448cbd32b49beb767efd315
Felipe Mascarin <felipe_mascarin@hotmail.com>
2022-08-24 00:30:35 -0300

    Desenvolvimento inicial do primeiro exercício de introdução à POO.

---
+++ PExercicioGetSetPoo/Pessoa.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PExercicioGetSetPoo
+{
+    internal class Pessoa
+    {
+        private String nome;
+        private String nascimento;
+        private String cpf;
+        private String telefone;
+
+        //Método Construtor
+        public Pessoa(String nome, String data_nasc, String cpf, String telefone)
+        {
+            this.nome = nome;
+            this.nascimento = data_nasc;
+            this.cpf = cpf;
+            this.telefone = telefone;
+        }
+
+
+        //Métodos Getters
+        public String GetNome()
+        {
+            return this.nome;
+        }
+
+        public String GetNascimento()
+        {
+            return this.nascimento;
+        }
+
+        public String GetCpf()
+        {
+            return this.cpf;
+        }
+
+        public String GetTelefone()
+        {
+            return this.telefone;
+        }
+
+
+        //Métodos Setters
+
+        public void SetNome(String nome)
+        {
+            this.nome = nome;
+        }
+
+        public void SetNascimento(String nascimento)
+        {
+            this.nascimento = nascimento;
+        }
+
+        public void SetCpf(String cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        public void SetTelefone(String telefone)
+        {
+            this.telefone = telefone;
+        }
+
+
+        //Método ToString
+
+        public override String ToString()
+        {
+            return "Nome: " + this.nome + "\nData de Nascimento: " + this.nascimento + "\nCPF: " + this.cpf + "\nTelefone: " + this.telefone;
+        }
+    }
+}
--- PExercicioGetSetPoo/Program.cs
+++ PExercicioGetSetPoo/Program.cs
@@ -4,9 +4,43 @@ namespace PExercicioGetSetPoo
 {
     internal class Program
     {
+        /*Programa que vai instanciar 3 objetos que venham de uma classe chamada Pessoa, onde essa classe contém 4 atributos:
+        Nome
+        Data de Nasc
+        CPF
+        Telefone
+
+        Construtores, getters, setters, ToString;
+
+        Usuário informa os atributos para o método construtor;
+
+        */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nome, data_nasc, cpf, telefone;
+            int qtd_pessoas = 3;            
+            Pessoa[] vet = new Pessoa[qtd_pessoas];
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write("Pessoa " + (i+1) + ": Informe o nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Pessoa " + (i + 1) + ": Informe a data de nascimento: ");
+                data_nasc = Console.ReadLine();
+                Console.Write("Pessoa " + (i + 1) + ": informe o CPF: ");
+                cpf = Console.ReadLine();
+                Console.Write("Pessoa " + (i + 1) + ": Informe o Telefone: ");
+                telefone = Console.ReadLine();
+                
+                vet[i] = new Pessoa(nome, data_nasc, cpf, telefone);
+                
+                Console.WriteLine(vet[i].ToString()+"\n");                
+            }
+
+            /* Código não possui tratamento de inserção de dados, apenas possui os requisitos solicitados do
+             exercício para praticar introdução a POO.*/
+
         }
     }
 }
