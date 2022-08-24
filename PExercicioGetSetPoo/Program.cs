cd0f6a7ffb1974408a09513b0f5ef770a6bb7392
Felipe Mascarin <felipe_mascarin@hotmail.com>
2022-08-24 00:10:01 -0300

    Adicionar arquivos de projeto.

---
+++ PExercicioGetSetPoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PExercicioGetSetPoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
